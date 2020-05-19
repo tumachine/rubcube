453d45f771fba3f019cad52e07816d5012648f9b
Tumachine <tumachiner@gmail.com>
2020-05-19 21:35:24 +0300

    removing directories

--- build/rubik/moveActions.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MoveActions = (function () {
-    function MoveActions() {
-    }
-    return MoveActions;
-}());
-exports.default = MoveActions;
-//# sourceMappingURL=moveActions.js.map
